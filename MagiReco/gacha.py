38b34e1f021f511f2bcdd2af5a840fe452245b76
Glenn Dawson <trebledawson@gmail.com>
2019-09-20 00:17:36 -0400

    Multiprocessing for expectation. Removed spooks.
    Spooks can be determined analytically using a negative binomial distribution, so simulating them is unnecessary.

--- MagiReco/gacha.py
+++ MagiReco/gacha.py
@@ -1,6 +1,6 @@
 # #################################################################### #
 # gacha.py                                                             #
-# Author: Glenn Dawson (2019)                                          #
+# Author: picardyThird (2019)                                          #
 # -----                                                                #
 # This file performs a Monte Carlo simulation of a large number of     #
 # rolls on an event banner in the game Magia Record. The objective is  #
@@ -119,7 +119,7 @@ def draw_n(n_rolls, ten_draws=True):
                 pity = 0  # Reset pity
                 if random.randint(0, 99) < 60:  # Banner
                     n_banner += 1
-            else:  # Rolls 90 - 99
+            else:  # Rolls 91 - 99
                 if random.randint(0, 99) == 0:  # SSR success
                     pity = 0  # Reset pity
                     if random.randint(0, 99) < 60:  # Banner
@@ -151,39 +151,27 @@ def expected_rolls():
         k = int(k)
     
     start = time.time()
-    rolls = []
-    spooks = []
     
-    while len(rolls) < k:
-        n_rolls, n_spooks = find_ssr(n, ten_draws)
-        rolls.append(n_rolls)
-        spooks.append(n_spooks)
-        if len(rolls) % 1000 == 0:
-            print('Trials:', len(rolls), '| Sample rolls:', n_rolls, 
-                  '| Sample spooks:', n_spooks)
+    p = Pool()
+    rolls = p.map(find_ssr, [(i, n, ten_draws) for i in range(k)])
+    p.close()
+    p.join()
         
     avg = mean(rolls)
     confidence_999 = 3.291 * (stdev(rolls) / sqrt(k))
     print('Average number of rolls required to get', n,
           'copies of the banner SSR over', k, 'trials: {0:.2f} +/- '
           '{1:.3f} (99.9% confidence)'.format(avg, confidence_999))
-      
-    avg_s = mean(spooks)
-    confidence_999_s = 3.291 * (stdev(spooks) / sqrt(k))
-    print('Average number of spooks drawn while rolling for', n,
-          'copies of the banner SSR over', k, 'trials: {0:.2f} +/- '
-          '{1:.3f} (99.9% confidence)'.format(avg_s, confidence_999_s))
     
     print('Script runtime: {:.8f} seconds.'.format(time.time() - start))
 
 
-def find_ssr(n=4, ten_draws=True):
+def find_ssr(args):
     n_banner = 0
-    n_spook = 0
     rolls = 0
     pity = 0
-    while n_banner < n:
-        if ten_draws:
+    while n_banner < args[1]:
+        if args[2]:
             if pity < 90:  # Do ten-draws below 90
                 for ten_draw in range(10):
                     roll = random.randint(0, 99)
@@ -194,27 +182,11 @@ def find_ssr(n=4, ten_draws=True):
                             pity = 0  # Reset pity
                             if random.randint(0, 99) < 60:  # Banner
                                 n_banner += 1
-                                if VERBOSE:
-                                    print('Banner', n_banner, 
-                                          'found in', rolls, 'rolls.')
-                            else:  # Spook!
-                                n_spook += 1
-                                if VERBOSE:
-                                    print('Spooked! Total spooks:', 
-                                          n_spook)
                     else:  # 10th roll has 2% chance
                         if roll <= 1:  # SSR Success
                             pity = 0  # Reset pity
                             if random.randint(0, 99) < 60:  # Banner
                                 n_banner += 1
-                                if VERBOSE:
-                                    print('Banner', n_banner, 
-                                          'found in', rolls, 'rolls.')
-                            else:  # Spook!
-                                n_spook += 1
-                                if VERBOSE:
-                                    print('Spooked! Total spooks:', 
-                                          n_spook)
             else:  # Do one-rolls from 90-100
                 pity += 1
                 rolls += 1
@@ -222,27 +194,11 @@ def find_ssr(n=4, ten_draws=True):
                     pity = 0  # Reset pity
                     if random.randint(0, 99) < 60:  # Banner
                         n_banner += 1
-                        if VERBOSE:
-                            print('Banner', n_banner, 
-                                  'found in', rolls, 'rolls.')
-                    else:  # Spook!
-                        n_spook += 1
-                        if VERBOSE:
-                            print('Spooked! Total spooks:', 
-                                  n_spook)
                 else:  # Rolls 90 - 99
                     if random.randint(0, 99) == 0:  # SSR success
                         pity = 0  # Reset pity
                         if random.randint(0, 99) < 60:  # Banner
                             n_banner += 1
-                            if VERBOSE:
-                                print('Banner', n_banner, 
-                                      'found in', rolls, 'rolls.')
-                        else:  # Spook!
-                            n_spook += 1
-                            if VERBOSE:
-                                print('Spooked! Total spooks:', 
-                                      n_spook)
         else:
             pity += 1
             rolls += 1
@@ -250,28 +206,15 @@ def find_ssr(n=4, ten_draws=True):
                 pity = 0  # Reset pity
                 if random.randint(0, 99) < 60:  # Banner
                     n_banner += 1
-                    if VERBOSE:
-                        print('Banner', n_banner, 
-                              'found in', rolls, 'rolls.')
-                else:  # Spook!
-                    n_spook += 1
-                    if VERBOSE:
-                        print('Spooked! Total spooks:', 
-                              n_spook)
             else:  # Rolls 90 - 99
                 if random.randint(0, 99) == 0:  # SSR success
                     pity = 0  # Reset pity
                     if random.randint(0, 99) < 60:  # Banner
                         n_banner += 1
-                        if VERBOSE:
-                            print('Banner', n_banner, 
-                                  'found in', rolls, 'rolls.')
-                    else:  # Spook!
-                        n_spook += 1
-                        if VERBOSE:
-                            print('Spooked! Total spooks:', 
-                                  n_spook)
-    return rolls, n_spook
+    if args[0] % 1000 == 0:
+        print('Trials:', args[0], '| Sample rolls:', rolls)
+        
+    return rolls
 
 
 def plot_gacha():        
@@ -299,6 +242,14 @@ def plot_gacha():
     plt.ylabel('Number of copies')
     plt.title(
         'Expected Number of Copies of Banner Magical Girl in K Pulls')
+    plt.xlim(-10, 1010)
+    
+    plt.figure()
+    plt.plot(1 - data.iloc[:, 0])
+    plt.grid()
+    plt.xlabel('Number of draws')
+    plt.ylabel('Probability')
+    plt.title('Probability of Drawing At Least One Copy of the Banner Girl in K Pulls')
     plt.xlim(-10, 1010)
     plt.show()
     
